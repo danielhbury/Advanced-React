b65bfb5a4bd8174ea8bad44e8cfbd20540f790a7
Daniel Bury <daniel.bury@aexp.com>
2019-02-22 16:06:39 -0700

    first module done

---
+++ sick-fits/frontend/components/Page.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Header from './Header';
+import Meta from './Meta';
+
+class Page extends Component {
+  render() {
+    const { children } = this.props;
+    return (
+      <div>
+        <Meta />
+        <Header />
+        {children}
+      </div>
+    )
+  }
+}
+
+export default Page;
